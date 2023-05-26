bf9d9865663a68b60f5c67e405961c59e7ed2fae
Jacek Cz <jacek.czernecki123@gmail.com>
2023-05-26 17:07:07 +0200

    update and delete measurement by ID

--- server/controllers/bodyParamsController.js
+++ server/controllers/bodyParamsController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const {bodyParamsSchema} = require("../models/BodyParams");
+const mongoose = require("mongoose");
 
 
 const getLatestAll = async (req, res) => {
@@ -32,7 +33,7 @@ const getLatestAll = async (req, res) => {
 }
 
 
-const paramExists = (req,res,next) => {
+const paramExists = (req, res, next) => {
     const params = Object.keys(bodyParamsSchema.obj);
     if (params.includes(req.params.param)) {
         next()
@@ -41,21 +42,18 @@ const paramExists = (req,res,next) => {
             message: "Invalid body parameter"
         })
     }
-
 }
 
 
-
-const getMeasurement = async (req,res) => {
+const getMeasurement = async (req, res) => {
     const {param} = req.params;
     try {
-        const user = await User.findOne({login:req.user},
+        const user = await User.findOne({login: req.user},
             {[`bodyParameters.${param}`]: 1, _id: 0,}
         );
 
         res.json(user.bodyParameters[param]);
-    }
-    catch(e) {
+    } catch (e) {
         res.status(500).json({message: e.message});
     }
 }
@@ -63,33 +61,74 @@ const getMeasurement = async (req,res) => {
 const addMeasurement = async (req, res) => {
     const {param} = req.params;
     const {size} = req.body;
-    if(!req?.body?.size || typeof req.body.size !== 'number') {
+    if (!req?.body?.size || typeof req.body.size !== 'number') {
         return res.status(400).json({message: "Size is required or is not a number"});
     }
     try {
-        const user = await User.findOne({login:req.user});
+        const user = await User.findOne({login: req.user});
         user.bodyParameters[param] = [...user.bodyParameters[param], {size}];
         const result = await user.save();            //save in database
         res.json(result);
-    }
-    catch(e) {
+    } catch (e) {
         res.status(500).json({message: e.message});
     }
 }
 
-const changeMeasurement = async (req,res) => {
+const changeMeasurement = async (req, res) => {
+    const {param} = req.params;
+    let {id, size} = req.body;
+    if (!req?.body?.id) {
+        return res.status(400).json({message: "ID is required"});
+    }
+    if (!req?.body?.size || typeof req.body.size !== 'number') {
+        return res.status(400).json({message: "Size is required or is not a number"});
+    }
+    try {
+        const user = await User.findOne({login: req.user});
+        console.log(user)
+        const record = user.bodyParameters[param].find(obj => {
+          return  obj._id.equals(new mongoose.Types.ObjectId(id));
+        });
+        if (!record) {
+            return res.sendStatus(204); //no content
+        }
+        record.size = size;
+        const result = await user.save();
+
+        res.json({record, result});
+    } catch (e) {
+        res.status(500).json({message: e.message});
+    }
+
 }
-const deleteMeasurement = async (req,res) => {}
+const deleteMeasurement = async (req, res) => {
+    const {param} = req.params;
+    let {id} = req.body;
+    if (!req?.body?.id) {
+        return res.status(400).json({message: "ID is required"});
+    }
+    try {
+        const user = await User.findOne({login: req.user});
+        let record = user.bodyParameters[param].find(obj => {
+            return  obj._id.equals(new mongoose.Types.ObjectId(id));
+        });
+        if (!record) {
+            return res.sendStatus(204); //no content
+        }
+        user.bodyParameters[param] =user.bodyParameters[param].filter(obj => !obj.equals(record));
+        const result = await user.save();
 
-    // try {
-    //     const foundUser = await User.findOne({login: req.user}).exec();
-    //     console.log(foundUser);
-    //     foundUser.bodyParameters.hips = [...foundUser.bodyParameters.hips, {size: 999}];
-    //     const result = await foundUser.save();
-    // } catch (e) {
-    //     console.log(e.message)
-    // }
+        res.json({record, result});
+    } catch (e) {
+        res.status(500).json({message: e.message});
+    }
+}
 
 
-
-module.exports = {getLatestAll, addMeasurement, getMeasurement: [paramExists, getMeasurement], changeMeasurement, deleteMeasurement};
+module.exports = {
+    getLatestAll,
+    addMeasurement,
+    getMeasurement: [paramExists, getMeasurement],
+    changeMeasurement,
+    deleteMeasurement
+};
