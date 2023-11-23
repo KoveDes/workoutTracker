0a9b583a9b7cee81c26fcd3d42d64e768159e12c
Jacek Czernecki <jacek.czernecki123@gmail.com>
2023-11-23 15:41:54 +0100

    Deployment preparation

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: 'localhost',
-    // port: 5173, //dev server on 5173
     proxy: {
       "/api": {
-        target: 'http://localhost:3500',
+        // target: 'http://localhost:3500',
+        target: 'http://localhost:10000', //deploy test
         changeOrigin: true,
         rewrite: path => path.replace('/api', ''),
         cookiePathRewrite: {
